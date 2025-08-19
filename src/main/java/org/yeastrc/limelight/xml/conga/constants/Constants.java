0a2f140384984ce4d8070e28c0d1e31e47eb3ea7
Dan Jaschob <djaschob@uw.edu>
2025-08-19 13:58:24 -0700

    Update to output version based on GIT release tag
    The gradle build uses environment variable 'LIMELIGHT_RELEASE_TAG'.
    The Github Action build sets 'LIMELIGHT_RELEASE_TAG' from the GIT
    release tag:
    
       env:
       	  LIMELIGHT_RELEASE_TAG: ${{ github.event.release.tag_name }}

--- src/main/java/org/yeastrc/limelight/xml/conga/constants/Constants.java
+++ src/main/java/org/yeastrc/limelight/xml/conga/constants/Constants.java
@@ -6,7 +6,9 @@ public class Constants {
 
 	public static final String CONVERSION_PROGRAM_NAME = "congaToLimelightXML.jar";
 	public static final String CONVERSION_PROGRAM_URI = "https://github.com/yeastrc/limelight-import-conga";
-	public static final String CONVERSION_PROGRAM_VERSION = "1.2.1";
+	
+	//  Removed as now retrieved from file which is populated from environment variable which is populated from GIT release tag
+//	public static final String CONVERSION_PROGRAM_VERSION = "1.2.1";
 
 	public static final String DEFAULT_STATIC_MOD_STRING = "C:57.02146";
 
--- src/main/java/org/yeastrc/limelight/xml/conga/main/MainProgram.java
+++ src/main/java/org/yeastrc/limelight/xml/conga/main/MainProgram.java
@@ -22,6 +22,8 @@ package org.yeastrc.limelight.xml.conga.main;
 import org.yeastrc.limelight.xml.conga.constants.Constants;
 import org.yeastrc.limelight.xml.conga.objects.ConversionParameters;
 import org.yeastrc.limelight.xml.conga.objects.ConversionProgramInfo;
+import org.yeastrc.limelight.xml.conga.utils.Limelight_GetVersion_FromFile_SetInBuildFromEnvironmentVariable;
+
 import picocli.CommandLine;
 
 import java.io.BufferedReader;
@@ -30,7 +32,7 @@ import java.io.InputStreamReader;
 
 @CommandLine.Command(name = "java -jar " + Constants.CONVERSION_PROGRAM_NAME,
 		mixinStandardHelpOptions = true,
-		version = Constants.CONVERSION_PROGRAM_NAME + " " + Constants.CONVERSION_PROGRAM_VERSION,
+		versionProvider = LimelightConverterVersionProvider.class,
 		sortOptions = false,
 		synopsisHeading = "%n",
 		descriptionHeading = "%n@|bold,underline Description:|@%n%n",
@@ -82,7 +84,20 @@ public class MainProgram implements Runnable {
 			System.exit( 1 );
 		}
 
-		ConversionProgramInfo cpi = ConversionProgramInfo.createInstance( String.join( " ",  args ) );        
+		ConversionProgramInfo cpi = null;
+		
+		try {
+			cpi = ConversionProgramInfo.createInstance( String.join( " ",  args ) );        
+		} catch(Throwable t) {
+
+			System.err.println("Error running conversion: " + t.getMessage());
+
+			if(verboseRequested) {
+				t.printStackTrace();
+			}
+
+			System.exit(1);
+		}
 
 		ConversionParameters cp = new ConversionParameters();
 		cp.setConversionProgramInfo( cpi );
@@ -129,7 +144,7 @@ public class MainProgram implements Runnable {
 			while ( ( line = br.readLine() ) != null ) {
 
 				line = line.replace( "{{URL}}", Constants.CONVERSION_PROGRAM_URI );
-				line = line.replace( "{{VERSION}}", Constants.CONVERSION_PROGRAM_VERSION );
+				line = line.replace( "{{VERSION}}", Limelight_GetVersion_FromFile_SetInBuildFromEnvironmentVariable.getVersion_FromFile_SetInBuildFromEnvironmentVariable() );
 
 				System.err.println( line );
 				
